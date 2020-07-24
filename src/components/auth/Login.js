8f60c9e5c1b572bf658875e2e6d7361b43b9ca0c
Dylan <dachaconp@unal.edu.co>
2020-07-24 11:06:57 -0500

    Login structure

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    //state
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+    const { email, password } = user;
+    //funciones
+    const onChange = e => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+    }
+    const onSubmit = e => {
+        e.preventDefault()
+    }
     return (
-        <h1>Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+
+                <form
+                    onSubmit={onSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={email}
+                            placeholder="Correo electrónico"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">contraseña</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={password}
+                            placeholder="Contraseña"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <button
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                        >Iniciar Sesión</button>
+                    </div>
+                </form>
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
+            </div> 
+        </div>
     );
 }
  
