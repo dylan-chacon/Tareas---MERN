bd7f7c0e45797dd49d6e68c8045aaff03db5c586
Dylan <dachaconp@unal.edu.co>
2020-07-27 11:19:29 -0500

    show projects with context

--- src/components/proyectos/List.js
+++ src/components/proyectos/List.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Project from './Project'
+import projectContext from '../../context/proyectos/projectContext';
 
 const List = () => {
-    const projects = [
-        {name: 'abc'}
-    ]
+    //context
+    const projectsContext = useContext(projectContext);
+    const { projects, getProjects } = projectsContext;
+    //useEffect
+    useEffect(() => {
+        getProjects();
+    });
+    //Revisar contenido
+    if (projects.length === 0) return null;
+
     return (
         <ul className="listado-proyectos">
             {projects.map(project => (
                 <Project
+                    key={project.id}
                     project={project}
                 />
             ))}
--- src/context/proyectos/projectState.js
+++ src/context/proyectos/projectState.js
@@ -1,25 +1,44 @@
 import React, { useReducer } from 'react';
 import projectContext from './projectContext';
 import projectReducer from './projectReducer';
-import { FORM_PROJECT } from '../../types';
+import {
+    FORM_PROJECT,
+    GET_PROJECTS
+} from '../../types';
 
 const ProjecState = props => {
+    const eProjects = [
+        {name: 'Lets Goal', id: 1},
+        {name: 'AllMarket', id: 2},
+        {name: 'Dr. Faustus', id: 3},
+    ]
     const initialState = {
         form: false,
+        projects: [],
     }
-    //dispatch actions
+//dispatch actions
     const [state, dispatch] = useReducer(projectReducer, initialState);
-    //CRUD functions
+//CRUD functions
+    //show form
     const showForm = () => {
         dispatch({
             type: FORM_PROJECT
-        })
+        });
+    }
+    //get projects
+    const getProjects = () => {
+        dispatch({
+            type: GET_PROJECTS,
+            payload: eProjects
+        });
     }
     return (
         <projectContext.Provider
             value={{
                 form: state.form,
-                showForm
+                projects: state.projects,
+                showForm,
+                getProjects
             }}
         >
             {props.children}
